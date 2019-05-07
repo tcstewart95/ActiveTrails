e577b6941db1e43ac1147816c82c32a3a93f3e91
Tevin C Stewart <tevincs@ad.it.et.byu.edu>
2019-05-07 10:15:09 -0600

    Changed the int counter to 5

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 10;
+  int _counter = 5;
 
   void _incrementCounter() {
     setState(() {
